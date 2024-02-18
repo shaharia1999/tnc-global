6040c0d2a123c5725c834abbec07689dd9cf4abf
MD Shaharia <adeptshaharia@gmail.com>
2024-02-18 16:05:24 +0600

    footerAdded

---
+++ src/app/component/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <p className="bg-red-500 py-2 text-white mt-10 text-center">Copyright by @shaharia</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Banner from "./component/Banner";
+import Footer from "./component/Footer";
 import Movie from "./component/Movie";
 export default function Home() {
   return (
     <main className="">
      <Banner/>
      <Movie/>
+     <Footer/>
     </main>
   );
 }
